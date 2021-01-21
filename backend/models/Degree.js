d8ae81cfec0bb36755ac7f3e914ba9867ffb5dca
algoroloro <algoro9697@gmail.com>
2021-01-21 18:28:12 +0100

    Degree model

---
+++ backend/models/Degree.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Degree = new mongoose.Schema(
+    {
+        degree: {
+            type: String,
+            required: true,
+        },
+        academy: {
+            type: String,
+            required: true,
+        },
+        done: {
+            type: String,
+            required: true,
+        },
+        startDate: {
+            type: Date,
+            required: true,
+        },
+        endDate: {
+            type: Date,
+            required: true,
+        }
+    },
+    {timestamps: true}
+);
+
+const model = mongoose.model('Degree', Degree);
+module.exports = model;
