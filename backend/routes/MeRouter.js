256badcf2b2edb07ba2ad9e09f9ac0d1672d3d2d
algoroloro <algoro9697@gmail.com>
2021-01-21 18:48:17 +0100

    Me router

---
+++ backend/routes/MeRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const MeController = require('../controllers/MeController.js');
+
+const router = express.Router();
+
+router.post('/create', MeController.create);
+router.get('/read', MeController.read);
+router.put('/update', MeController.update);
+router.put('/delete', MeController.delete);
+
+module.exports = router;
