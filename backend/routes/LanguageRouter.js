e3966c882c66f1f55e1f33aa28d2397cc610c874
algoroloro <algoro9697@gmail.com>
2021-01-21 18:18:43 +0100

    Language router

---
+++ backend/routes/LanguageRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const LanguageController = require('../controllers/LanguageController.js');
+
+const router = express.Router();
+
+router.post('/create', LanguageController.create);
+router.get('/read', LanguageController.read);
+router.put('/update', LanguageController.update);
+router.put('/delete', LanguageController.delete);
+
+module.exports = router;
