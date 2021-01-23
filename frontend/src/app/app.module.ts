23717181dfc5b00d824a0d0cf7c2f8ec3d9a6fd1
algoroloro <algoroloro@gmail.com>
2021-01-23 11:41:00 +0100

    Creating first routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from 'src/app/app.component';
 import { AppRouting } from 'src/app/app.routing';
-import { HeaderComponent } from '@core/header/header.component';
-import { NavigatorComponent } from '@core/navigator/navigator.component';
 import { FooterComponent } from '@core/footer/footer.component';
+import { HeaderComponent } from '@core/header/header.component';
+import { HomeComponent } from '@core/home/home.component';
+import { NavigatorComponent } from '@core/navigator/navigator.component';
+import { PageNotFoundComponent } from '@core/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
     HeaderComponent,
+    HomeComponent,
     NavigatorComponent,
-    FooterComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from '@core/home/home.component';
+import {PageNotFoundComponent} from '@core/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {data: {title: ''}, path: 'home', component: HomeComponent},
+  {path: '**', redirectTo: 'page-not-found', pathMatch: 'full'},
+  {data: {title: ''}, path: '404', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRouting {}
+export class AppRouting {
+}
