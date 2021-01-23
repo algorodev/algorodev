e155908b2ead0adc5cf060091587d049b37f520e
algoroloro <algoroloro@gmail.com>
2021-01-23 12:09:25 +0100

    Title service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TitleService } from '@shared/services/title.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [TitleService],
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  constructor(private titleService: TitleService) {}
+
+  ngOnInit(): void {
+    this.titleService.initializeTitleService();
+  }
 }
--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,18 +1,25 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from '@core/home/home.component';
-import {PageNotFoundComponent} from '@core/page-not-found/page-not-found.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '@core/home/home.component';
+import { PageNotFoundComponent } from '@core/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {data: {title: ''}, path: 'home', component: HomeComponent},
-  {path: '**', redirectTo: 'page-not-found', pathMatch: 'full'},
-  {data: {title: ''}, path: '404', component: PageNotFoundComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    data: { title: 'Full-Stack Developer' },
+    path: 'home',
+    component: HomeComponent,
+  },
+  { path: '**', redirectTo: '404', pathMatch: 'full' },
+  {
+    data: { title: 'Page not found' },
+    path: '404',
+    component: PageNotFoundComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRouting {
-}
+export class AppRouting {}
