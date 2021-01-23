46ee952b9b7481b317a66a65b9e0bb0dc137c696
algoroloro <algoroloro@gmail.com>
2021-01-23 11:21:08 +0100

    Import route alias

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from 'src/app/app.component';
 import { AppRouting } from 'src/app/app.routing';
-import { HeaderComponent } from 'src/app/core/header/header.component';
-import { NavigatorComponent } from 'src/app/core/navigator/navigator.component';
-import { FooterComponent } from 'src/app/core/footer/footer.component';
+import { HeaderComponent } from '@core/header/header.component';
+import { NavigatorComponent } from '@core/navigator/navigator.component';
+import { FooterComponent } from '@core/footer/footer.component';
 
 @NgModule({
   declarations: [
